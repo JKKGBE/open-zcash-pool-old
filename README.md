7a9666b0b4ad72e53ae34b00e3979122cfcb2435
Jakub Kowalski <jakubkowalskigbe@gmail.com>
2018-06-06 19:58:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Open Zcash Pool README placeholder
+
+Based on sammy007's Open Ethereum Pool
---
+++ rpc/rpc.go
@@ -0,0 +1,95 @@
+package rpc
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"sync"
+
+	"github.com/jkkgbe/open-zcash-pool/util"
+)
+
+type RPCClient struct {
+	sync.RWMutex
+	Url         string
+	Name        string
+	sick        bool
+	sickRate    int
+	successRate int
+	client      *http.Client
+}
+
+type JSONRpcResp struct {
+	Id     *json.RawMessage       `json:"id"`
+	Result *json.RawMessage       `json:"result"`
+	Error  map[string]interface{} `json:"error"`
+}
+
+func NewRPCClient(name, url, timeout string) *RPCClient {
+	rpcClient := &RPCClient{Name: name, Url: url}
+	timeoutIntv := util.MustParseDuration(timeout)
+	rpcClient.client = &http.Client{
+		Timeout: timeoutIntv,
+	}
+	return rpcClient
+}
+
+func (r *RPCClient) GetBlockTemplate() ([]string, error) {
+	rpcResp, err := r.doPost(r.Url, "getblocktemplate", []string{})
+	if err != nil {
+		return nil, err
+	}
+	var reply []string
+	err = json.Unmarshal(*rpcResp.Result, &reply)
+	return reply, err
+}
+
+func (r *RPCClient) SubmitBlock(params []string) (bool, error) {
+	rpcResp, err := r.doPost(r.Url, "submitblock", params)
+	if err != nil {
+		return false, err
+	}
+	var reply bool
+	err = json.Unmarshal(*rpcResp.Result, &reply)
+	return reply, err
+}
+
+func (r *RPCClient) doPost(url string, method string, params interface{}) (*JSONRpcResp, error) {
+	jsonReq := map[string]interface{}{"jsonrpc": "2.0", "method": method, "params": params, "id": 0}
+	data, _ := json.Marshal(jsonReq)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	req.Header.Set("Content-Length", (string)(len(data)))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := r.client.Do(req)
+	if err != nil {
+		r.markSick()
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var rpcResp *JSONRpcResp
+	err = json.NewDecoder(resp.Body).Decode(&rpcResp)
+	if err != nil {
+		r.markSick()
+		return nil, err
+	}
+	if rpcResp.Error != nil {
+		r.markSick()
+		return nil, errors.New(rpcResp.Error["message"].(string))
+	}
+	return rpcResp, err
+}
+
+func (r *RPCClient) markSick() {
+	r.Lock()
+	r.sickRate++
+	r.successRate = 0
+	if r.sickRate >= 5 {
+		r.sick = true
+	}
+	r.Unlock()
+}
