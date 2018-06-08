2047fc958b587a822a5362b17529de5d6b56125a
Unknown <k.michalak.gajda@gmail.com>
2018-06-08 10:59:13 +0200

    Small adjustment

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -129,7 +129,7 @@ func NewProxy(cfg *Config, backend *storage.RedisClient) *ProxyServer {
 }
 
 func (s *ProxyServer) Start() {
-	log.Printf("Starting proxy on %v", s.config.Proxy.Listen)
+	log.Printf("Starting proxy on %v 🤙", s.config.Proxy.Listen)
 	r := mux.NewRouter()
 	r.Handle("/{login:t[0-9a-fA-F]{34}}/{id:[0-9a-zA-Z-_]{1,20}}", s)
 	r.Handle("/{login:t[0-9a-fA-F]{34}}", s)
